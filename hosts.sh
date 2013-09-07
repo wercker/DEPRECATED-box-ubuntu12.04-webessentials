0b33d463c44c603becccc0303ba22ae42f8bb3a3
Jacco Flenter <github@putpixel.nl>
2013-09-07 09:56:54 +0200

    github/bitbucket hosts added

---
+++ hosts.sh
@@ -0,0 +1,9 @@
+mkdir -p $HOME/.ssh
+known_hosts_path="$HOME/.ssh/known_hosts"
+
+touch $known_hosts_path
+chmod 600 $known_hosts_path
+
+sudo pip install netaddr
+./add_hosts $known_hosts_path
+sudo pip uninstall netaddr
